5c0faf6d911e57246f6dca7cce0938a0b5a09989
morf8 <49359518+morf8@users.noreply.github.com>
2019-04-25 11:54:59 +0300

    feat: add new spacing styling rules at index.js file (#2)
    BREAKING CHANGE: you have to fit your code style according to this rules, otherwise you'll get errors.
    
    * add spacing styling rules at index.js file
    * increase the major version
    
    The rules are:
    * 1 empty line maximum between code snippets
    * spacing after comma
    * spacing before block types: functions, classes and keywords(keywords like if {, else { etc..)
    * spacing after colon
    * line breaks after opening and before closing array brackets if the array's lines are breaks.
    * consistent spacing inside braces of object literals, destructuring assignments, and import/export specifiers
    * spacing before and after arrow functions.
    * spacing before keywords of block (for example: } else)
    * brace style - the open bracket of a block need to be at the same time of the keyword.

--- index.js
+++ index.js
@@ -18,5 +18,14 @@ module.exports = {
     semi: ['error', 'always'],
     'no-console': 'off',
     eqeqeq: ['error', 'always'],
+    'no-multiple-empty-lines': [2, { max: 1 }],
+    'comma-spacing': [2, { before: false, after: true }],
+    'space-before-blocks': [2, { functions: "always", classes: "always", keywords: "always" }],
+    'key-spacing': [2, { afterColon: true }],
+    'array-bracket-newline': [2, { multiline: true }],
+    'object-curly-spacing': [2, "always"],
+    'arrow-spacing': [2, { before: true, after: true }],
+    'keyword-spacing': [2, { before: true }],
+    'brace-style': [2, "1tbs"]
   },
 };
